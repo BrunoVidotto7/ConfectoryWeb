d3cefa0f298e5a9ea513be2b7572212e55983cdd
zutmrba <zutmrba@ericsson.com>
2022-01-24 13:09:32 -0300

    Starting project

---
+++ src/main/java/br/com/confectoryWeb/ConfectoryWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.confectoryWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfectoryWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfectoryWebApplication.class, args);
+
+	}
+}
